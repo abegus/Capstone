8be19ba156f0b6cee1538df6c28f64ad7d5290a5
abegus <agbues@gmail.com>
2017-12-25 10:00:59 -0600

    C# to Json converter

---
+++ Capstone/Helpers/JavaScriptConverter.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Helpers
+{
+    public static class JavaScriptConvert
+    {
+        public static IHtmlString SerializeObject(object value)
+        {
+            using (var stringWriter = new StringWriter())
+            using (var jsonWriter = new JsonTextWriter(stringWriter))
+            {
+                var serializer = new JsonSerializer
+                {
+                    // Let's use camelCasing as is common practice in JavaScript
+                    ContractResolver = new CamelCasePropertyNamesContractResolver()
+                };
+
+                // We don't want quotes around object names
+                jsonWriter.QuoteName = false;
+                serializer.Serialize(jsonWriter, value);
+
+                return new HtmlString(stringWriter.ToString());
+            }
+        }
+    }
+    /*    USAGE EXAMPLE
+     *    <script>
+             var pirates = @JavaScriptConvert.SerializeObject(Model.Pirates);
+            </script>
+
+     * <script>
+        var pirates = [{firstName:"Jack",lastName:"Sparrow"},{firstName:"Will",lastName:"Turner"},{firstName:"Elizabeth",lastName:"Swann"}];
+        </script>
+     * 
+     * 
+     */
+}
