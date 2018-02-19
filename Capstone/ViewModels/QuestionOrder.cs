2c4318dd776e39c1e0e82fc133e0f2e743ef6b18
abegus <agbues@gmail.com>
2018-02-19 15:53:33 -0600

    Finished Question Reordering

---
+++ Capstone/ViewModels/QuestionOrder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.ViewModels
+{
+	public class QuestionOrder
+	{
+		
+        public string QuestionId { get; set; }
+        public int QuestionIndex { get; set; }
+	}
+}
